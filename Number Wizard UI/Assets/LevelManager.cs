a233e7db574f8df443981be9c56b26750d0657c9
perrywil <william.perry.r@gmail.com>
2018-01-09 21:00:52 +0100

    Number Wizard UI
    Start screen, title, subtitle and creation of buttons

---
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name){
+		Debug.Log("Level load requested for " + name);
+	}
+
+	public void QuitRequest (){
+		Debug.Log("Quit the game");
+	}
+	
+}
