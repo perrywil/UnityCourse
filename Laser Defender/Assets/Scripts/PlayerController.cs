b741acebef364fa99fdd6bea2aa0625524e28672
perrywil <william.perry.r@gmail.com>
2018-01-29 23:38:37 +0100

    Projectiles
    projectiles move forward when space key is pressed and destroy
    themselves when they are out of the screen

--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+	public GameObject projectile;
 	public float speed = 15.0f;
 	public float padding = 0.7f;
+	public float beamSpeed = 5f;
+	public float firingRate = 0.2f;
 
 	float xmin = -5;
 	float xmax = 5;
@@ -18,14 +20,26 @@ public class PlayerController : MonoBehaviour {
 		xmin = leftmost.x + padding;
 		xmax = righttmost.x - padding;
 	}
-	
+	void Fire () {
+		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = Vector3.up * beamSpeed;
+	}
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.0000001f, firingRate);
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke("Fire");
+		}
+
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
 	// restrict the player to the game-space
 	float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
 	transform.position = new Vector3(newX, transform.position.y, transform.position.z);
