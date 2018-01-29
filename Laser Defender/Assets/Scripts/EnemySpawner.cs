84a25b27cd5bf369aa4ce04ce2ef9ed48d6909b3
perrywil <william.perry.r@gmail.com>
2018-01-29 22:19:14 +0100

    Enemy formation
    enemy formation moves from left to right on the screen

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	public float speed = 5f;
+
+	private bool movingRight = true;
+	private float xmin;
+	private float xmax;
 
 	// Use this for initialization
 	void Start () {
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 LeftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distanceToCamera));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distanceToCamera));
+		xmax = rightBoundary.x;
+		xmin = LeftBoundary.x;
 		foreach (Transform child in transform) {
 			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.transform.parent = child;
 		}
 	}
+
+	public void OnDrawGizmos (){
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
+	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update (){
+		if (movingRight) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		} else {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+
+		float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+		float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+		if (leftEdgeOfFormation < xmin) {
+			movingRight = true;
+		} else if(rightEdgeOfFormation > xmax){
+			movingRight = false;
+		}
 	}
 }
