4a310d2eb23e5a8e071532729fa0e5c8c09160b2
perrywil <william.perry.r@gmail.com>
2018-01-31 20:44:15 +0100

    Laser trigger
    laser triggers with enemy ships created

---
+++ Laser Defender/Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float damage = 100;
+
+
+	public void OnBecameInvisible() {
+    Destroy(gameObject);
+	}
+
+	public float GetDamage (){
+		return damage;
+	}
+
+	public void Hit (){
+		Destroy(gameObject);
+	}
+		
+}
