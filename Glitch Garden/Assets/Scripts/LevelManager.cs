57b946e9d0adc1ea1d9d9df98096d78efc86c90c
perrywil <william.perry.r@gmail.com>
2018-02-04 20:33:53 +0100

    Glitch Garden
    Splash screen + main menu created

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start (){
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel (string name){
+		Debug.Log("Level load requested for " + name);
+		//Application.LoadLevel(name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest (){
+		Debug.Log("Quit the game");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+		//Application.LoadLevel(Application.loadedLevel + 1);
+		// Loadlevel is outdated, its recommended to use SceneManager
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
