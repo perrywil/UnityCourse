980cb3a5877ab48a0b65c0b0151c19aa6b9d7796
perrywil <william.perry.r@gmail.com>
2018-02-16 18:14:36 +0100

    animations and other corrections

---
+++ Glitch Garden/Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+
+	private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+		//find music manager
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+		if (musicManager) {
+			Debug.Log ("Found music manager " + musicManager);
+			float volume = PlayerPrefsManager.GetMasterVolume();
+			musicManager.ChangeVolume(volume);
+		} else {
+			Debug.LogWarning ("no music manager found in Start scene, can't set volume");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
