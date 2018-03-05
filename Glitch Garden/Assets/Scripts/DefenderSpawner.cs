1a47cbd2301341a44702b6e899c6c719e5fd32f6
perrywil <william.perry.r@gmail.com>
2018-03-05 19:33:27 +0100

    created defenderSpawner
    now selected defenders can be spawned in the gameplay screen

---
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+	public Camera myCamera;
+
+	private GameObject defenderParent;
+
+
+	void Start () {
+		defenderParent = GameObject.Find("Defenders");
+
+		if (!defenderParent) {
+			defenderParent = new GameObject("Defenders");
+		}
+	}
+
+	void OnMouseDown () {
+		//print (Input.mousePosition);
+		//print (SnapToGrid(CalculateWorldPointOfMouseClick ()));
+		Vector2 rawPos = CalculateWorldPointOfMouseClick();
+		Vector2 roundedPos = SnapToGrid (rawPos);
+		GameObject defender = Button.selectedDefender;
+		Quaternion zeroRot = Quaternion.identity;
+		GameObject newDef = Instantiate (defender, roundedPos, zeroRot) as GameObject;
+
+		newDef.transform.parent = defenderParent.transform;
+	}
+
+	Vector2 SnapToGrid (Vector2 rawWorldPos) {
+		float newX = Mathf.RoundToInt(rawWorldPos.x);
+		float newY = Mathf.RoundToInt(rawWorldPos.y);
+		return new Vector2 (newX, newY);
+	}
+
+
+	Vector2 CalculateWorldPointOfMouseClick () {
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+		float distanceFromCamera = 10f;
+
+		Vector3 wierdTriplet = new Vector3 (mouseX, mouseY, distanceFromCamera);
+		Vector2 worldPos = myCamera.ScreenToWorldPoint (wierdTriplet);
+
+		return worldPos;
+	}
+
+}
+
--- Glitch Garden/Assets/Scripts/button.cs
+++ Glitch Garden/Assets/Scripts/button.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class button : MonoBehaviour {
+public class Button : MonoBehaviour {
 
 	public GameObject defenderPrefab;
 	public static GameObject selectedDefender;
 
+	private Button[] buttonArray;
+
 	
 	// Use this for initialization
 	void Start () {
+		buttonArray = GameObject.FindObjectsOfType<Button>();
 	}
 	
 	// Update is called once per frame
@@ -18,15 +21,21 @@ public class button : MonoBehaviour {
 		
 	}
 
-	void OnMouseDown () {
+	void OnMouseDown ()
+	{
 		//Debug.Log (name + " is pressed");
-	
-		GetComponent<SpriteRenderer>().color = Color.white;
-	}
 
-	void OnMouseUp () {
-		GetComponent<SpriteRenderer>().color = Color.black;
+		foreach (Button thisButton in buttonArray) {
+			thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+		}
+		GetComponent<SpriteRenderer>().color = Color.white;
 		selectedDefender = defenderPrefab;
 		print (selectedDefender);
 	}
+
+	//void OnMouseUp () {
+	//	GetComponent<SpriteRenderer>().color = Color.black;
+	//	selectedDefender = defenderPrefab;
+	//	print (selectedDefender);
+	//}
 }
