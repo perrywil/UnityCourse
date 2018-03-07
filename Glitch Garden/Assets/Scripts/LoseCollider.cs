08422e80692d66016c1c02b5fcbd6c6125584efb
perrywil <william.perry.r@gmail.com>
2018-03-07 17:53:27 +0100

    created Lose condition
    Lose box collider sends player to lose screen if attacker crosses the
    screen

---
+++ Glitch Garden/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+	
+	// Use this for initialization
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D ()	{
+	 	//print ("Attackers win");
+		levelManager.LoadLevel("03 a Lose Screen");
+	}
+}
