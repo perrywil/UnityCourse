c33a00bcbb0baf8076663b706c45f9dcc4e78f5f
perrywil <william.perry.r@gmail.com>
2018-03-07 23:32:44 +0100

    created time slider
    a slider that sets the time of the game level in order to pass the level

---
+++ Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	public float levelSeconds = 100;
+
+	//public float secondsLeft; // TODO make secondsLeft private once its working
+	private Slider slider;
+	private AudioSource audioSource;
+	private bool isEndOfLevel = false;
+	private LevelManager levelManager;
+	private GameObject winText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		slider = GetComponent<Slider> ();
+		audioSource = GetComponent<AudioSource> ();
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+		FindYouWin ();
+		winText.SetActive(false);
+		//secondsLeft = levelSeconds;
+	}
+
+	void FindYouWin () {
+		winText = GameObject.Find ("You Win");
+		if (!winText) {
+			Debug.LogWarning ("create You Win");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// calculates the advancement of the slider when secondsLeft are reduced.
+		// no need to use secondsLeft any more since Time.timeSinceLevelLoad calculates 
+		//automatically the time left since the level starts
+		slider.value = Time.timeSinceLevelLoad / levelSeconds;
+
+		bool timeIsUp = (Time.timeSinceLevelLoad >= levelSeconds);
+		// isEndOfLevel is used so we just get a single call
+		if (timeIsUp && !isEndOfLevel){
+			audioSource.Play();
+			winText.SetActive(true);
+			Invoke ("LoadNextLevel", audioSource.clip.length);
+			isEndOfLevel = true;
+		}
+	}
+
+	void LoadNextLevel () {
+		levelManager.LoadNextLevel();
+	}
+
+}
