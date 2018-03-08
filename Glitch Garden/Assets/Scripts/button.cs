4d139c987b558a75db54fdf6905c3c5b7e896b73
perrywil <william.perry.r@gmail.com>
2018-03-08 18:09:36 +0100

    final tweeks in the game

--- Glitch Garden/Assets/Scripts/button.cs
+++ Glitch Garden/Assets/Scripts/button.cs
@@ -9,11 +9,17 @@ public class Button : MonoBehaviour {
 	public static GameObject selectedDefender;
 
 	private Button[] buttonArray;
-
+	private Text costText;
 	
 	// Use this for initialization
 	void Start () {
 		buttonArray = GameObject.FindObjectsOfType<Button>();
+
+		costText = GetComponentInChildren<Text>();
+		if(!costText){
+			Debug.LogWarning(name + "Has no text component to add cost");
+		}
+		costText.text = defenderPrefab.GetComponent<Defender>().starCost.ToString();
 	}
 	
 	// Update is called once per frame
