b3553daf14a749b7cea5c292c9d6532375faa5b7
perrywil <william.perry.r@gmail.com>
2018-03-12 18:33:59 +0100

    ball moves throgh lane
    rigidbody and audiosource added to ball plus script to make it move,
    also camera moves with ball, created a cameracontrol script

---
+++ Bowlmaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    //public float launchSpeed;
+    public Vector3 launchVelocity;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+
+    }
+
+    public void Launch() {
+        rigidBody.velocity = launchVelocity;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Bowlmaster/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Ball ball;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // we have to calculate the distance of the camera from the ball
+        offset = transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // the transform of the camera changes as the transform of the ball
+        // making the camera move towards the pins with the ball
+        if (transform.position.z <= 1829f) { // in fron of head pin
+            transform.position = ball.transform.position + offset;
+        }
+        
+	}
+}
