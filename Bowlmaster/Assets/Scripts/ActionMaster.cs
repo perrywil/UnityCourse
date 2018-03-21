724722929148bb9ce5b82b8aea69b8bf102521c3
perrywil <william.perry.r@gmail.com>
2018-03-21 19:44:03 +0100

    ActionMaster created
    tests made to check code is correct

--- Bowlmaster/Assets/Scripts/ActionMaster.cs
+++ Bowlmaster/Assets/Scripts/ActionMaster.cs
@@ -6,18 +6,50 @@ public class ActionMaster { // this class is for test purposes and we do not nee
 
     public enum Action {Tidy, Reset, EndTurn, EndGame};
 
+    private int[] bowls = new int[21];
+    private int bowl = 1;
+
     public Action Bowl(int pins) {
-        if (pins < 0 || pins > 10)
-        {
+        if (pins < 0 || pins > 10) {
             throw new UnityException("invalid pin count!");
         }
 
-        if (pins == 10)
+        bowls[bowl - 1] = pins;
+
+        if (bowl == 21)
         {
+            return Action.EndGame;
+        }
+
+        //Handle last-frame special cases
+        if (bowl >= 19 && Bowl21Awarded())
+        {
+            bowl += 1;
+            return Action.Reset;
+        } else if (bowl == 20 && !Bowl21Awarded())
+        {
+            return Action.EndGame;
+        }
+
+        if (pins == 10) {
+            bowl += 2;
             return Action.EndTurn;
         }
         // other behaviur here
 
+        //if first bowl of frame, return Action.Tidy;
+        if (bowl % 2 != 0) { //Mid frame (or last frame)
+            bowl += 1;
+            return Action.Tidy;
+        } else if(bowl % 2 == 0){ //End of frame
+            bowl += 1;
+            return Action.EndTurn;
+        }
+
         throw new UnityException("Not sure what action to return!");
+    }
+
+    private bool Bowl21Awarded() {
+        return (bowls[19 - 1] + bowls[20 - 1] >= 10);
     }
 }
