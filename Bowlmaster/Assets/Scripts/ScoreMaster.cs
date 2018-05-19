3751fcf55a9b0c65b9bfbca81f9b56dda5778353
perrywil <william.perry.r@gmail.com>
2018-05-19 22:02:01 +0200

    added scoreMaster Test

--- Bowlmaster/Assets/Scripts/ScoreMaster.cs
+++ Bowlmaster/Assets/Scripts/ScoreMaster.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class ScoreMaster {
 
+    //Returns a list of cumulative scores, like a normal score card
+    public static List<int> ScoreCumulative(List<int> rolls)
+    {
+        List<int> cumulativeScores = new List<int>();
+        int runningTotal = 0;
+
+        foreach (int frameScore in ScoreFrames(rolls))
+        {
+            runningTotal += frameScore;
+            cumulativeScores.Add(runningTotal);
+        }
+
+        return cumulativeScores;
+    }
+
+    //Return a list of individual frame scores, Not cumulative
     public static List<int> ScoreFrames (List<int> rolls)
     {
         List<int> frameList = new List<int>();
