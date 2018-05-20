417d54a6d025e0e7a2fab41edfa9da2427255202
perrywil <william.perry.r@gmail.com>
2018-05-20 13:45:46 +0200

    more refactoring

--- Bowlmaster/Assets/Scripts/ScoreMaster.cs
+++ Bowlmaster/Assets/Scripts/ScoreMaster.cs
@@ -22,10 +22,31 @@ public class ScoreMaster {
     //Return a list of individual frame scores, Not cumulative
     public static List<int> ScoreFrames (List<int> rolls)
     {
-        List<int> frameList = new List<int>();
+        List<int> frames = new List<int>();
 
-        // Code here
+        // Index i points to 2nd bowl of frame
+        for(int i = 1; i <  rolls.Count; i += 2)    
+        {
+            if (frames.Count == 10) { break; }                  // prevents 11th frame score
 
-        return frameList;
+            if(rolls[i-1] + rolls[i] < 10)                      // Normal "OPEN" frame
+            { 
+            frames.Add(rolls[i - 1] + rolls[i]);
+            }
+
+            if (rolls.Count - i <= 1) { break; }                // Ensure at least one look-ahead available
+
+            if(rolls[i-1] == 10)                                // STRIKE
+            {
+                i--;                                            // Strike frame has just one bowl
+                frames.Add(10 + rolls[i + 1] + rolls[i+2]);
+            } else if (rolls[i - 1] + rolls[i] == 10)           // Calculate SPARE bonus
+            {
+                frames.Add(10 + rolls[i + 1]);
+            }
+
+        }
+
+        return frames;
     }
 }
