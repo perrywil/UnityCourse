29737317ee73a2508eeb5676670eda18fd101e93
perrywil <william.perry.r@gmail.com>
2018-07-11 17:35:55 +0200

    [UI, UX, Functionality] added player, first enemy, Terrain decoration

---
+++ Zombie Runner/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject[] spawnPoints;
+    public bool reSpawn = false;
+
+    private GameObject currentPoint;
+    int index;
+    private bool lastToggle;
+
+	// Use this for initialization
+	void Start () {
+        ReSpawn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastToggle != reSpawn)
+        {
+            ReSpawn();
+            reSpawn = false;
+        } else
+        {
+            lastToggle = reSpawn;
+        }
+	}
+
+    private void ReSpawn()
+    {
+        index = Random.Range(0, spawnPoints.Length);
+        currentPoint = spawnPoints[index];
+        this.transform.position = currentPoint.transform.position;
+    }
+}
